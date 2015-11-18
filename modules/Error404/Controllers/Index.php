8f8a701d59ec78eb9fd128ee3c8af72da5e823ee
Tim G <timgarrity89@gmail.com>
2015-11-18 03:54:30 +0000

    So my texteditor rewrote a few files. I caught the error404.

--- modules/Error404/Controllers/Index.php
+++ modules/Error404/Controllers/Index.php
@@ -1,65 +1,24 @@
 <?php
-namespace Module\Admin\Controllers;
-use \Lyra\Controller;
+namespace Module\Error404\Controllers;
 /**
  * Index controller
  */
-class Index extends Controller
+class Index extends \Lyra\Controller
 {
-    private $players;
-    public function __construct()
-    {
-        \App::useAdminTheme();
-        $player = \App::getModel('session');
-		$checkPlayer = $player->isLoggedIn() && \Acl::hasRole('administrator');
-        \View::set('loggedIn', $checkPlayer);
-        if ($checkPlayer === false ){
-            header('Location: Home');
-            exit;
-        } else {
-            $playerID = $player->get('player_id');
-            \View::set('player', \App::getModel('Player')->find($playerID));
-        }
-        $this->players = \App::getModel('adminPlayers');
-        $this->loadStats();
-    }
+    /**
+     * Page title
+     * @var string
+     */
+    protected $title = 'Error 404';
     /**
      * Default action
-     * @param $args array
      */
-    public function index(array $args = array())
+    public function index()
     {
-        \View::setTemplate('Admin.twig');
-        \View::set('pageTitle', 'Admin Panel');
-    }
-    public function appearance()
-    {
-        \View::setTemplate('Admin.twig');
-        \View::set('pageTitle', 'Appearances');
-        \View::set('mainContent', 'Select Themes and stuff');
-    }
-    public function users()
-    {
-        \View::setTemplate('Users.twig');
-		$players = \App::getModel('Player');
-		\View::set('playerArray', $players->findAll());
-        \View::set('pageTitle', 'User Manager');
-        \View::set('mainContent', 'See stats, edit users, etc');
-    }
-    public function config()
-    {
-        \View::setTemplate('Admin.twig');
-        \View::set('pageTitle', 'Configuration');
-        \View::set('mainContent', 'See and edit the Site Config');
-    }
-    public function modules()
-    {
-        \View::setTemplate('Admin.twig');
-        \View::set('pageTitle', 'Module Manager');
-        \View::set('mainContent', 'Install/Activate/Deactive/Uninstall Modules');
-    }
-    public function loadStats()
-    {
-        \View::set('numPlayers', $this->players->getTotal());
+        if (!headers_sent()) {
+            header('HTTP/1.1 404 Not Found');
+            header('Status: 404 Not Found');
+        }
+		\View::setTemplate('Error.twig');
     }
 }
